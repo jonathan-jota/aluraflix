1603ae954d82bf17b9c7c4ca645d4b48d0637fc4
Jonathan <jonathan.jota.santos@gmail.com>
2020-08-01 02:29:54 -0300

    Update Aula 05

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 
-const server = jsonServer.crate();
+const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
--- src/index.js
+++ src/index.js
@@ -3,28 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// eslint-disable-next-line import/no-named-as-default-member
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-
+// Desafio master blaster na descrição
 const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/cadastro/video" component={CadastroVideo} />
-        <Route path="/" component={CadastroCategoria} />
-        <Route component={Pagina404} />
-
-      </Switch>
-    </BrowserRouter>,
-
-document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
-//<React.StrictMode>
-  //<App />
-//</React.StrictMode>,
