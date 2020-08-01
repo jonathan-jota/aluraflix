a3a4ea4ef8972fa5d0f5b3b743999db0f1ad57c3
Jonathan <jonathan.jota.santos@gmail.com>
2020-08-01 01:14:58 -0300

    Update Heroku

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Button = styled.button`
     color: var(--white);
     border: 1px solid var(--white);
+    background: var(--black);
     box-sizing: border-box;
     cursor: pointer;
     padding: 16px 24px;
@@ -19,5 +20,6 @@ const Button = styled.button`
     &:focus {
     opacity: .5;
 }
-`
+`;
+
 export default Button;
